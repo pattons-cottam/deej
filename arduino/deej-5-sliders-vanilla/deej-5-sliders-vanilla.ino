586a747dc982f9feb1444b70f28ff7d0abe1237c
Ben <pattons-cottam@protonmail.com>
2021-02-06 20:01:01 +0000

    support for rotary encoders

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,52 +1,68 @@
-const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+#include <ClickEncoder.h>
+#include <TimerOne.h>
 
-int analogSliderValues[NUM_SLIDERS];
+const int NUM_ENCODERS = 2;
 
-void setup() { 
-  for (int i = 0; i < NUM_SLIDERS; i++) {
-    pinMode(analogInputs[i], INPUT);
-  }
+ClickEncoder *encoder1;
+ClickEncoder *encoder2;
 
-  Serial.begin(9600);
+int encoderValues[NUM_ENCODERS];
+
+void timerIsr()
+{
+  encoder1->service();
+  encoder2->service();
 }
 
-void loop() {
-  updateSliderValues();
-  sendSliderValues(); // Actually send data (all the time)
-  // printSliderValues(); // For debug
+void updateEncoderValues()
+{
+  encoderValues[0] += encoder1->getValue();
+  encoderValues[1] += encoder2->getValue();
+
+  for (int i = 0; i < NUM_ENCODERS; i++)
+  {
+    if (encoderValues[i] < 0)
+    {
+      encoderValues[i] = 0;
+    }
+
+    if (encoderValues[i] > 100)
+    {
+      encoderValues[i] = 100;
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  encoder1 = new ClickEncoder(A1, A0, A2); // CLK, DT, SW (button)
+  encoder2 = new ClickEncoder(A4, A3, A2); // CLK, DT, SW (button)
+
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(timerIsr);
+
+  encoder1->setAccelerationEnabled(true);
+  encoder2->setAccelerationEnabled(true);
+
   delay(10);
 }
 
-void updateSliderValues() {
-  for (int i = 0; i < NUM_SLIDERS; i++) {
-     analogSliderValues[i] = analogRead(analogInputs[i]);
-  }
-}
+void loop()
+{
+  updateEncoderValues();
 
-void sendSliderValues() {
   String builtString = String("");
 
-  for (int i = 0; i < NUM_SLIDERS; i++) {
-    builtString += String((int)analogSliderValues[i]);
+  for (int i = 0; i < NUM_ENCODERS; i++)
+  {
+    builtString += String(map(encoderValues[i], 0, 100, 0, 1023)); // convert to analog equivalent
 
-    if (i < NUM_SLIDERS - 1) {
+    if (i < NUM_ENCODERS - 1)
+    {
       builtString += String("|");
     }
   }
-  
+
   Serial.println(builtString);
-}
-
-void printSliderValues() {
-  for (int i = 0; i < NUM_SLIDERS; i++) {
-    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV");
-    Serial.write(printedString.c_str());
-
-    if (i < NUM_SLIDERS - 1) {
-      Serial.write(" | ");
-    } else {
-      Serial.write("\n");
-    }
-  }
 }
