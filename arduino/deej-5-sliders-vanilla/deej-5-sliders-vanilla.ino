c33c95aa6a1b65d4f7db9e41d8cc6472f465b78a
Ben <pattons-cottam@protonmail.com>
2021-02-07 17:56:37 +0000

    added two encoders on the digital pins

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,14 +1,22 @@
 #include <ClickEncoder.h>
 #include <TimerOne.h>
 
-const int NUM_ENCODERS = 2;
+const int NUM_ENCODERS = 5;
 
 // a lower multiplier means more granular control
 const int MASTER_STEP_MULTIPLIER = 3;
 const int STEP_MULTIPLIER = 5;
 
+static const uint8_t D2 = 2;
+static const uint8_t D3 = 3;
+static const uint8_t D4 = 4;
+static const uint8_t D5 = 5;
+
 ClickEncoder *encoder1;
 ClickEncoder *encoder2;
+ClickEncoder *encoder3;
+ClickEncoder *encoder4;
+ClickEncoder *encoder5;
 
 int encoderValues[NUM_ENCODERS];
 
@@ -16,12 +24,18 @@ void timerIsr()
 {
   encoder1->service();
   encoder2->service();
+  encoder3->service();
+  encoder4->service();
+  encoder5->service();
 }
 
 void updateEncoderValues()
 {
   encoderValues[0] += encoder1->getValue() * MASTER_STEP_MULTIPLIER;
   encoderValues[1] += encoder2->getValue() * STEP_MULTIPLIER;
+  encoderValues[2] += encoder3->getValue() * STEP_MULTIPLIER;
+  encoderValues[3] += encoder4->getValue() * STEP_MULTIPLIER;
+  encoderValues[4] += encoder5->getValue() * STEP_MULTIPLIER;
 
   for (int i = 0; i < NUM_ENCODERS; i++)
   {
@@ -40,16 +54,22 @@ void updateEncoderValues()
 void setup()
 {
   Serial.begin(9600);
+  // ClickEncoder(DT, CLK, SW (button)); // swap DT and CLK here if encoders are backwards
+  encoder1 = new ClickEncoder(A1, A0, A7); // master
+  encoder2 = new ClickEncoder(A3, A2, A7);
+  encoder3 = new ClickEncoder(A5, A4, A7);
 
-  // ClickEncoder(CLK, DT, SW (button, not supported));
-  encoder1 = new ClickEncoder(A1, A0, A2);
-  encoder2 = new ClickEncoder(A4, A3, A2);
+  encoder4 = new ClickEncoder(D3, D2, A7);
+  encoder5 = new ClickEncoder(D5, D4, A7);
 
   Timer1.initialize(1000);
   Timer1.attachInterrupt(timerIsr);
 
   encoder1->setAccelerationEnabled(true);
   encoder2->setAccelerationEnabled(true);
+  encoder3->setAccelerationEnabled(true);
+  encoder4->setAccelerationEnabled(true);
+  encoder5->setAccelerationEnabled(true);
 
   // the sliders actually control their volume as a proportion of the master
   // volume, i.e spotify.exe at 100% will be 25% on the Windows volume
